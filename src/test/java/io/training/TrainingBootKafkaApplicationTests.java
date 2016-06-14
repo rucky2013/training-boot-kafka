5f49696d05123aece20b560fba34062766aede56
Percy0601 <2008dreams@2008.sina.com>
2016-06-14 14:35:42 +0800

    初始化项目

---
+++ src/test/java/io/training/TrainingBootKafkaApplicationTests.java
@@ -0,0 +1,16 @@
+package io.training;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TrainingBootKafkaApplication.class)
+public class TrainingBootKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
